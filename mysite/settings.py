45696d91a2034c8b94f9cd51d58c5ea71fef37f1
Darla Shockley <darla@fly.io>
2024-10-08 15:05:39 +0200

    update settings for running on fly

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,23 +26,13 @@ SECRET_KEY = 'django-insecure-vuri3cmicsje6&$!ak11_05s$%0htcj&rhqj29m6ndu*o@g-jq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# The `DYNO` env var is set on Heroku CI, but it's not a real Heroku app, so we have to
-# also explicitly exclude CI:
-# https://devcenter.heroku.com/articles/heroku-ci#immutable-environment-variables
-IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
+IS_FLY_APP = "FLY_APP_NAME" in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU_APP:
+if not IS_FLY_APP:
     DEBUG = True
 
-# On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
-# validation of the Host header in the incoming HTTP request. On other platforms you may need to
-# list the expected hostnames explicitly in production to prevent HTTP Host header attacks. See:
-# https://docs.djangoproject.com/en/5.0/ref/settings/#std-setting-ALLOWED_HOSTS
-if IS_HEROKU_APP:
-    ALLOWED_HOSTS = ["*"]
-else:
-    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
+ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", ".fly.dev"]
 
 # Application definition
 
